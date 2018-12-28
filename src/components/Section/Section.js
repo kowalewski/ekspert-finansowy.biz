723bf263e6efe4f9e596556cf5833ffab7edc3a0
Radoslaw Kowalewski <kowalewski.r@gmail.com>
2018-12-28 15:42:42 +0100

    services list

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { node, oneOf } from 'prop-types';
+import { node, oneOf, string } from 'prop-types';
 
-export default function Section({ children, modificator }) {
+export default function Section({ title, intro, children, modificator }) {
     const modificatorClass = modificator ? `section--${modificator}` : '';
-    return <div className={`section ${modificatorClass}`}>{children}</div>;
+    return (
+        <div className={`section ${modificatorClass}`}>
+            {title && <h3 className="section__title">{title}</h3>}
+            {intro && (
+                <div className="section__intro">
+                    <p className="section__ingress">{intro}</p>
+                </div>
+            )}
+            {children}
+        </div>
+    );
 }
 
 Section.propTypes = {
     children: node,
     modificator: oneOf(['dark']),
+    title: string,
+    intro: string,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import LatestPosts from '../components/LatestPosts/LatestPosts';
 import Intro from '../components/Intro/Intro';
 import HeaderButtons from '../components/HeaderButtons/HeaderButtons';
 import Footer from '../components/Footer/Footer';
+import Section from '../components/Section/Section';
+import Services from '../components/Services/Services';
+import servicesData from '../components/Services/servicesData';
 
 export default class IndexPage extends Component {
     static propTypes = {
@@ -25,6 +28,12 @@ export default class IndexPage extends Component {
                 <Intro />
                 <HeaderButtons />
                 <main className="main-content full-height">
+                    <Section
+                        title="Chciałbyś wiedzieć w czym mogę Ci pomóc?"
+                        intro="Jako ekspert finansowy oferuję Państwu profesjonalną pomoc w zakresie finansów i ubezpieczeń. Swoją działalność opieram na pośredniczeniu w udzielaniu wszelkiego rodzaju kredytów, a także leasingów. Kompletuję od Państwa niezbędną dokumentację do procesowania wniosku kredytowego i przeprowadzam Państwa przez cały etap okołokredytowy. Dysponuję ofertą ponad 16 renomowanych banków, co pozwala na dobranie jak najkorzystniejszej oferty do Państwa potrzeb. Oprócz tego, jako agent ubezpieczeniowy, jestem w stanie pomóc Państwu wybrać odpowiednią ofertę ubezpieczenia na życie spośród cenionych na polskim rynku firm ubezpieczeniowych."
+                    >
+                        <Services services={servicesData} />
+                    </Section>
                     <LatestPosts posts={posts} />
                     <Footer />
                 </main>
