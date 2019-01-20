93d55659fea6d7b2f2a496f92cb7ce01f5ee7bc7
Radoslaw Kowalewski <kowalewski.r@gmail.com>
2019-01-20 23:21:28 +0100

    base styling for tags page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -16,9 +16,9 @@ class TagRoute extends React.Component {
         const tag = this.props.pageContext.tag;
         const title = this.props.data.site.siteMetadata.title;
         const totalCount = this.props.data.allMarkdownRemark.totalCount;
-        const tagHeader = `${totalCount} post${
+        const tagHeader = `${totalCount} posty ${
             totalCount === 1 ? '' : 's'
-        } tagged with “${tag}”`;
+        } oznaczone tagiem: “${tag}”`;
 
         return (
             <Layout>
@@ -30,12 +30,12 @@ class TagRoute extends React.Component {
                                 className="column is-10 is-offset-1"
                                 style={{ marginBottom: '6rem' }}
                             >
-                                <h3 className="title is-size-4 is-bold-light">
+                                <h3 className="tags__title">
                                     {tagHeader}
                                 </h3>
                                 <ul className="taglist">{postLinks}</ul>
                                 <p>
-                                    <Link to="/tags/">Browse all tags</Link>
+                                    <Link to="/tags/">Zobacz wszystkie tagi</Link>
                                 </p>
                             </div>
                         </div>
